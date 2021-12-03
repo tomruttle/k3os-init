607bcff4a32acda808b78762257fee9df657e17a
Tom Ruttle <tom.ruttle@thoughtworks.com>
2021-12-03 22:31:20 +0100

    working config! move to terraform

--- main.tf
+++ main.tf
@@ -11,6 +11,21 @@ provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
 module "dashboard" {
   source = "./modules/dashboard"
+}
+
+module "traefik" {
+  source = "./modules/traefik"
+}
+
+module "blocky" {
+  depends_on = [module.traefik]
+  source = "./modules/blocky"
 }
--- modules/traefik/main.tf
+++ modules/traefik/main.tf
@@ -1,162 +1,68 @@
-resource "kubernetes_service_account" "traefik" {
-  metadata {
-    name      = "traefik"
-    namespace = "kube-system"
-    labels    = { app : "traefik" }
-  }
-}
+resource "helm_release" "traefik" {
+  name      = "traefik"
+  namespace = "kube-system"
 
-resource "kubernetes_deployment" "traefik" {
-  metadata {
-    name      = "traefik"
-    namespace = "kube-system"
-    labels    = { app : "traefik" }
+  repository = "https://helm.traefik.io/traefik"
+  chart      = "traefik"
+
+  set {
+    name  = "rbac.enabled"
+    value = true
   }
 
-  spec {
-    replicas = 1
-
-    selector {
-      match_labels = {
-        app = "traefik"
-      }
-    }
-
-    template {
-      metadata {
-        labels = { app = "traefik" }
-      }
-
-      spec {
-        serviceAccountName = kubernetes_service_account.traefik.metadata.0.name
-
-        priority_class_name = "system-cluster-critical"
-
-        toleration {
-          kkey     = "CriticalAddonsOnly"
-          operator = "Exists"
-        }
-
-        toleration {
-          key      = "node-role.kubernetes.io/control-plane"
-          operator = "Exists"
-          effect   = "NoSchedule"
-        }
-
-        toleration {
-          key      = "node-role.kubernetes.io/master"
-          operator = "Exists"
-          effect   = "NoSchedule"
-        }
-
-        container {
-          name  = "traefik"
-          image = "traefik:v2.2"
-
-          args = [
-            "--api.insecure",
-            "--accesslog",
-            "--entrypoints.web.Address=:80",
-            "--entrypoints.websecure.Address=:443",
-            "--providers.kubernetescrd",
-            "--certificatesresolvers.myresolver.acme.tlschallenge",
-            "--certificatesresolvers.myresolver.acme.email=foo@you.com",
-            "--certificatesresolvers.myresolver.acme.storage=acme.json",
-          ]
-
-          port {
-            name           = "web"
-            container_port = 80
-            protocol       = "TCP"
-          }
-
-          port {
-            name           = "websecure"
-            container_port = 443
-            protocol       = "TCP"
-          }
-
-          port {
-            name           = "admin"
-            container_port = 8080
-            protocol       = "TCP"
-          }
-
-          #     rbac:
-          #       enabled: true
-          #     ports:
-          #       websecure:
-          #         tls:
-          #           enabled: true
-          #     podAnnotations:
-          #       prometheus.io/port: "8082"
-          #       prometheus.io/scrape: "true"
-          #     providers:
-          #       kubernetesIngress:
-          #         publishedService:
-          #           enabled: true
-
-          security_context {
-            run_as_group    = 0
-            run_as_non_root = false
-            run_run_as_user = 0
-
-            capabilities {
-              add = ["NET_BIND_SERVICE"]
-            }
-          }
-
-          # dashboard:
-          #   enabled: true
-          # ports:
-          #   traefik:
-          #     expose: true
-          #   blocky-udp:
-          #     name: blocky-udp
-          #     port: 53
-          #     protocol: UDP
-          #     expose: true
-          #   blocky-tcp:
-          #     name: blocky-tcp
-          #     port: 53
-          #     protocol: TCP
-          #     expose: true
-        }
-      }
-    }
-  }
-}
-
-resource "kubernetes_service" "traefik" {
-  metadata {
-    name      = "traefik"
-    namespace = "kube-system"
-    labels    = { app : "traefik" }
+  set {
+    name  = "dashboard.enabled"
+    value = true
   }
 
-  spec {
-    type = "LoadBalancer"
-
-    selector = {
-      app : kubernetes_deployment.traefik.spec.0.template.0.metadata.0.labels.app
-    }
-
-    port {
-      name     = "web"
-      port     = 80
-      protocol = "TCP"
-    }
-
-    port {
-      name     = "admin"
-      port     = 8080
-      protocol = "TCP"
-    }
-
-    port {
-      name     = "websecure"
-      port     = 443
-      protocol = "TCP"
-    }
+  set {
+    name  = "ports.traefik.expose"
+    value = true
   }
+
+  set {
+    name  = "ports.websecure.tls.enabled"
+    value = true
+  }
+
+  set {
+    name  = "providers.kubernetesIngress.publishedService.enabled"
+    value = true
+  }
+
+  set {
+    name  = "priorityClassName"
+    value = "system-cluster-critical"
+  }
+
+  values = [
+    <<EOT
+ports:
+  blocky-udp:
+    name: blocky-udp
+    port: 53
+    protocol: UDP
+    expose: true
+  blocky-tcp:
+    name: blocky-tcp
+    port: 53
+    protocol: TCP
+    expose: true
+securityContext:
+  capabilities:
+    add: [NET_BIND_SERVICE]
+  runAsGroup: 0
+  runAsNonRoot: false
+  runAsUser: 0
+tolerations:
+- key: "CriticalAddonsOnly"
+  operator: "Exists"
+- key: "node-role.kubernetes.io/control-plane"
+  operator: "Exists"
+  effect: "NoSchedule"
+- key: "node-role.kubernetes.io/master"
+  operator: "Exists"
+  effect: "NoSchedule"
+EOT
+  ]
 }
